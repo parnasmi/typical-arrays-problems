c7007c9da96ffc90ad77772c65e103c90799795f
Ilhom <parnasmi@gmail.com>
2020-10-03 21:02:24 +0500

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0)  return 0;
+  else return Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0) return 0;
+  else return Math.max(...array)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+  return arguments.length === 0 || array.length === 0 ? 0 : array.reduce((acc, current) => acc + current) / array.length;
 }
+
